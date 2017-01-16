026c122a9fcc767f405da99b325e04e77a2315a6
Pierre TINARD <Pierre TINARD>
2017-01-16 09:18:32 +0100

    Test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/default')
 
-@section('pagetitle') ChatProject @endsection
+@section('pagetitle') ChatProjesst @endsection
 
 @section('navbar')
 <li class="nav-item active"><a href="#" class="nav-link">Accueil</a></li>
