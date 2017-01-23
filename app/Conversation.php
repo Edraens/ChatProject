b0f15b95a7e40e102929cfc10bd2ead92859f8ea
Pierre Tinard <pierre@tinard.fr>
2017-01-23 08:54:46 +0100

    fix conv rel

--- app/Conversation.php
+++ app/Conversation.php
@@ -10,6 +10,6 @@ class Conversation extends Model
 
      public function destUser()
     {
-        return $this->hasOne('App\User', 'destId', 'id');
+        return $this->hasOne('App\User', 'id', 'destId');
     }
 }
