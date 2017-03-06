5e3a8534424bf1fef39f05e092c116e77f5f1947
Pierre Tinard <pierre@tinard.fr>
2017-03-06 08:38:26 +0100

    desing

--- resources/views/conversation.blade.php
+++ resources/views/conversation.blade.php
@@ -20,12 +20,11 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-sm-5">
-			<h2><b>{{ $conv->destUser->name }}</b> <i>({{ $conv->destUser->email }})</i></h2>
+		<div class="col-sm-6">
+			<h3><b>{{ $conv->destUser->name }}</b> <i>({{ $conv->destUser->email }})</i> <a href="/conversation/{{ $conv->id }}" class="btn btn-success"><i class="fa fa-lg fa-refresh"></i></a></h3>
 		</div>
-		<div class="col-xs-7">
+		<div class="col-xs-6">
 			<div class="pull-right">
-			<a href="/conversation/{{ $conv->id }}" class="btn btn-success"><i class="fa fa-lg fa-refresh"></i></a>
 				{{ $messages->links() }}
 			</div>
 		</div>
