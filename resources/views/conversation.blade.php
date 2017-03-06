b59c9fbc66d5030e0f2f6f567f6296eddc4db960
Pierre Tinard <pierre@tinard.fr>
2017-03-06 08:41:04 +0100

    span

--- resources/views/conversation.blade.php
+++ resources/views/conversation.blade.php
@@ -21,7 +21,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-6">
-			<h3><b>{{ $conv->destUser->name }}</b> <i>({{ $conv->destUser->email }})</i> <a href="/conversation/{{ $conv->id }}" class="btn btn-success"><i class="fa fa-lg fa-refresh"></i></a></h3>
+			<h3><b>{{ $conv->destUser->name }}</b> <i>({{ $conv->destUser->email }})</i> <span class="pull-right"><a href="/conversation/{{ $conv->id }}" class="btn btn-success"><i class="fa fa-lg fa-refresh"></i></a></span></h3>
 		</div>
 		<div class="col-xs-6">
 			<div class="pull-right">
