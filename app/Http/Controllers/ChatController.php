4d3fd0d3bb4fbef99abe9a0aa146e693f36732be
Pierre Tinard <pierre@tinard.fr>
2017-03-06 08:49:46 +0100

    fix lessages

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -252,7 +252,7 @@ class ChatController extends Controller
 		$convInfoUser = $conv->destUser->name;
 		$convInfomail = $conv->destUser->email;
 
-		foreach ($lessages as $message) {
+		foreach ($messages as $message) {
 			if ($message->fromUser == $user->id) $msgFromMe[$message->id] = true;
 			else $msgFromMe[$message->id] = false;
 		}
